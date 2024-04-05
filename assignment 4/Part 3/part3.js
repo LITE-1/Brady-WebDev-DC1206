b5d41e95076530e07cc3abc5060b109f9b4c3d25
LITE-1 <86115512+LITE-1@users.noreply.github.com>
2024-04-04 20:17:20 -0400

    canvas

--- assignment 4/Part 3/part3.js
+++ assignment 4/Part 3/part3.js
@@ -3,3 +3,8 @@
 // part3 assignment 4
 // 1206-01
 
+//Build canvas
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
