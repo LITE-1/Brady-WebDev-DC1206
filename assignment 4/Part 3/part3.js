6ac1c06da543fb22228c2b93f9edfcbd352c2395
LITE-1 <86115512+LITE-1@users.noreply.github.com>
2024-04-04 20:31:19 -0400

    Update part3.js

--- assignment 4/Part 3/part3.js
+++ assignment 4/Part 3/part3.js
@@ -56,4 +56,62 @@ class ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+
+
+collisionDetection() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
 }
+
+}
+
+  
+  const testBall = new Ball(50, 100, 4, 4, "red", 10);
+  
+  testBall.x;
+  testBall.size;
+  testBall.color;
+  testBall.draw();
+  
+  // animating the ball
+  const balls = [];
+  
+  while (balls.length < 25) {
+    const size = random(10, 20);
+    const ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size, width - size),
+      random(0 + size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      randomRGB(),
+      size,
+    );
+  
+    balls.push(ball);
+  }
+  
+  function loop() {
+    ctx.fillStyle = "rgb(0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
+  
+    for (const ball of balls) {
+      ball.draw();
+      ball.update();
+    }
+  
+    requestAnimationFrame(loop);
+  }
+  
+  
+  loop();
