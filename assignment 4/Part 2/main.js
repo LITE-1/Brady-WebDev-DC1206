ee440767fdce9759fc1a2596de35c411ec295c10
LITE-1 <86115512+LITE-1@users.noreply.github.com>
2024-04-04 20:04:03 -0400

    Update main.js

--- assignment 4/Part 2/main.js
+++ assignment 4/Part 2/main.js
@@ -5,10 +5,29 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFilesnames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
-
+const imageAltText = {
+    'pic1.jpg': 'Description for pic1',
+    'pic2.jpg': 'Description for pic2',
+    'pic3.jpg': 'Description for pic3',
+    'pic4.jpg': 'Description for pic4',
+    'pic5.jpg': 'Description for pic5'
+  };
 /* Looping through images */
+imageFilesnames.forEach(fileName => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${fileName}` );
+    newImage.setAttribute('alt', imageAltText[fileName]);
+    
+    thumbBar.appendChild(newImage);
+    
+    newImage.addEventListener('click',function(){
+        displayedImage.src = this.src;
+        displayedImage.alt = this.alt;
+    });  
+});
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
